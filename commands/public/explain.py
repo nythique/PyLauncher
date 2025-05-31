a62be775452eaebf8a3a481f400046fbb6bf9990
Nythique <nythique@proton.me>
2025-05-31 16:31:05 +0000

    Installer 1.13

--- commands/public/explain.py
+++ commands/public/explain.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from config.settings import GROQ_TOKEN
-import groq, os
+from config.settings import GROQ_TOKEN, MODEL, FREQUENCY, TEMPERATURE, MAX_TOKENS, TOP_P, PRESENCE_PENALTY
+from groq import Groq
 
 
 class Explain(commands.Cog):
@@ -11,22 +11,26 @@ class Explain(commands.Cog):
 
     @app_commands.command(name="explain", description="Donne une explication du code")
     @app_commands.describe(message="Le code à expliquer")
-    async def explain(self, interaction: discord.Interaction, message: str):
+    async def explain(self, interaction: discord.Interaction, code: str, language: str = "fr"):
         await interaction.response.defer(thinking=True, ephemeral=True)
         try:
-            client = groq.Groq(api_key=GROQ_TOKEN)
+            client = Groq(api_key=GROQ_TOKEN)
             prompt = (
-                "Explique simplement ce que fait ce code Python, ligne par ligne, en français :\n"
-                f"{message}\n\nExplication :"
+                f"Explique simplement ce que fait ce code Python, ligne par ligne, en {language} sans commentaire unile comme les salutation de ta part:\n"
+                f"{code}\n\nExplication :"
             )
             response = client.chat.completions.create(
-                model="llama3-8b-8192",  # Ou un autre modèle Groq disponible
+                model=MODEL,  
                 messages=[
-                    {"role": "system", "content": "Tu es un assistant Python qui explique le code simplement en français."},
+                    {"role": "system", "content": "Tu êtes un assistant qui explique le code Python de manière simple et claire. Sans commentaires inutiles."},
                     {"role": "user", "content": prompt}
                 ],
-                max_tokens=512,
-                temperature=0.2,
+                max_tokens=MAX_TOKENS,
+                top_p=TOP_P,
+                frequency_penalty=FREQUENCY,
+                presence_penalty=PRESENCE_PENALTY,
+                stop=["\n\n"], 
+                temperature=TEMPERATURE
             )
             explanation = response.choices[0].message.content.strip()
             embed = discord.Embed(
